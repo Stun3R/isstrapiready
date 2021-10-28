55970a3efa49277cc309c3ab644a9de7123d4966
Thibaut David <thibautdavid@icloud.com>
2021-10-28 14:12:11 +0200

    feat: add advancement section

---
+++ components/atoms/icons/CodeIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    width="32"
+    height="21"
+    viewBox="0 0 32 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0 12.787V7.48915L13.2446 0V5.0195L4.29495 10.1399L13.2446 15.2461V20.2787L0 12.787ZM18.7554 5.0195V0.000118075L32 7.49182V12.787L18.7554 20.2787V15.2461L27.7051 10.1399L18.7554 5.0195Z"
+      fill="white"
+    />
+  </svg>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
     <TheHeader />
+    <section
+      class="grid grid-cols-1 mx-6 mt-10  md:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-6 justify-items-center"
+    >
+      <AdvancementCard
+        v-for="{ attributes } in advancements.data"
+        :key="attributes.id"
+        :percent="attributes.percent"
+        :type="attributes.type"
+        :title="attributes.title"
+        class="w-full max-w-sm"
+      />
+    </section>
+    <section class="mx-6 mt-10"></section>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  async asyncData({ payload, app }) {
+    if (payload) return { ready: payload };
+    else {
+      const advancementsResponse = await axios.get(
+        `${process.env.STRAPI_URL}/advancements`
+      );
+      return { advancements: advancementsResponse.data };
+    }
+  },
+};
 </script>
 
 <style></style>
